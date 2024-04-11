d666ee40da2faa0e152e053cb7d67f82b84af6df
mery <meryhakobyan23@gmail.com>
2024-04-11 10:31:22 +0300

    swiper code commented

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
             link: [
                 // { rel: 'icon', type: 'image/x-icon', href: '/logoIcon.svg' },
                 { rel: 'stylesheet', type: 'text/css', href: '/lib/bootstrap/css/bootstrap.css?5' },
-                { rel: 'stylesheet', type: 'text/css', href: '/css/font.css' },
+                { rel: 'stylesheet', type: 'text/css', href: '/css/font.css?1' },
                 { rel: 'stylesheet', type: 'text/css', href: '/css/main.css' },
                 { rel: 'stylesheet', type: 'text/css', href: '/css/main1440.css' },
                 { rel: 'stylesheet', type: 'text/css', href: '/css/main1024.css' },
