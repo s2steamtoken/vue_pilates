d8aca404aec32af1c7cecf7fbf56da4df8f2952a
mery <meryhakobyan23@gmail.com>
2024-04-15 10:22:01 +0300

    form  contact-us

--- components/Themes/Theme27/index.vue
+++ components/Themes/Theme27/index.vue
@@ -1,121 +1,84 @@
 <script setup>
-
-const { $getImage, $sendApi } = useNuxtApp()
+const { $sendApi, $getImage, $clearFilter } = useNuxtApp()
 const globalData = inject('globalData')
 const translate = inject('translate')
 const Swal = inject('$swal')
-const $store = useNuxtApp()
+const formData = ref({
+    name:'',
+    name_2:'',
+    phone:'',
+    email: '',
+    comment:''
 
+})
 const validateErrors = ref({});
-
-const forgetpass = ref({
-    email: "",
-    code: "",
-    password: "",
-    repeat_password: ""
-});
-
-const router = useRouter()
-
-const step = ref(1)
-
-async function forgetPassword(e) {
-
-    e.preventDefault()
-
-    let result = await $sendApi(`api/user/forgetPassword `, forgetpass.value);
-
-    if (result.status == 500) {
-
-        if (result.message != undefined) {
+async function sendEmail(e) {
+    e.preventDefault();
+    let apiData = await $sendApi(`/api/forms/contact_us`, formData.value)
+    if (apiData.status != 200) {
+        validateErrors.value = apiData.errors;
+        if (apiData.message != undefined) {
             Swal.fire({
-                title: "Error",
-                text: result.message,
+                title: apiData.title,
+                text: apiData.message,
                 icon: 'error',
                 confirmButtonText: 'OK'
             });
-
-        }
-
-    }
-    else if (result.status == 201) {
-
-        console.log(forgetpass.value.code);
-        if (forgetpass.value.code != "") {
-            step.value = 3;
-        }
-        else {
-            step.value = 2
         }
     }
     else {
-
+        validateErrors.value = {};
         Swal.fire({
-            title: "Success",
-            text: result.message,
+            title: apiData.title,
+            text: apiData.message,
             icon: 'success',
             confirmButtonText: 'OK'
         });
-
-
-
-        router.push({
-            path: '/' + globalData.langUrl + 'sign-in',
-        })
     }
-    // else if(result.status == 201) {
-    // }
-    // else {
-    //     step.value == 3
-    //     router.push({
-    //         path: '/' + globalData.langUrl + 'forget-password',
-    //     })
-    // }
-
 }
-
 </script>
-
 <script>
 export default {
     props: {
         content: {},
         apiData: {},
-        cartData: {},
-        globalData: {},
     },
 };
-
 </script>
 <template>
     <section class="about-us-start">
         <div class="container">
             <div class="row justify-content-between align-items-center flex-direction-reverse">
                 <div class="col-lg-6 col-md-6 col-sm-6">
-                    <form class="about-us-form">
+                    <form class="about-us-form" @submit="sendEmail" id="contact_subscribe_form">
                         <div class="form-group mt-4">
                             <label>{{ translate('Name') }}</label>
-                            <input required="" type="text" class="form-control bg-gray-input" name="name">
+                            <input v-model="formData.name" type="text" class="form-control bg-gray-input" name="name">
+                            <div class="invalid-feedback">{{ validateErrors.name }}</div>
                         </div>
                         <div class="form-group mt-4">
                             <label>{{ translate('Surname') }}</label>
-                            <input required="" type="text" class="form-control bg-gray-input" name="lastname">
+                            <input v-model="formData.name_2" type="text" class="form-control bg-gray-input" name="lastname">
+                            <div class="invalid-feedback">{{ validateErrors.name_2 }}</div>
                         </div>
                         <div class="form-group mt-4">
                             <label>{{ translate('E-mail') }}</label>
-                            <input required="" type="text" class="form-control bg-gray-input" name="email">
+                            <input v-model="formData.email" type="text" class="form-control bg-gray-input" name="email">
+                            <div class="invalid-feedback">{{ validateErrors.email }}</div>
                         </div>
                         <div class="form-group mt-4">
                             <label>{{ translate('Phone') }}</label>
-                            <input required="" type="text" class="form-control bg-gray-input" name="phone">
+                            <input v-model="formData.phone" type="text" class="form-control bg-gray-input" name="phone">
+                            <div class="invalid-feedback">{{ validateErrors.phone }}</div>
                         </div>
                         <div class="form-group mt-4">
                             <label>{{ translate('Type Your Message') }}</label>
-                            <textarea required="" class="form-control bg-gray-input" rows="3" placeholder="Type..."
+                            <textarea v-model="formData.comment"  class="form-control bg-gray-input" rows="3" placeholder="Type..."
                                 name="message"></textarea>
+                                <div class="invalid-feedback">{{ validateErrors.comment }}</div>
                         </div>
                         <div class="d-flex mt-4 justify-content-between flex-wrap">
-                            <button class="btn-dark">{{ translate('SEND MESSAGE') }}</button>
+                            <button class="btn-dark" type="submit">{{ translate('SEND MESSAGE') }}</button>
                         </div>
                     </form>
                 </div>
@@ -126,30 +89,15 @@ export default {
                                 {{ content.title }}
                             </h2>
                         </div>
-                        <div class="contact-info-text">
-                            <h5 class="address-contact">Hanrapetutyan Hraparak</h5>
+                        <div class="contact-info-text"  v-for="item in globalData.contacts.address">
+                            <h5 class="address-contact"  v-if="item.address"> {{ item.address }}</h5>
                             <div class="d-flex mb-2 align-items-center grid-gap-part">
                                 <div class="contact-info-icon">
                                     <img src="/images/call.svg" alt="">
                                 </div>
-                                <div class="contact-info-number">
-                                    <a href="tel:+1(999)999-99-99" class="number">
-                                        +1(999)999-99-99
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="contact-info-text">
-
-                            <h5 class="address-contact">Hanrapetutyan Hraparak</h5>
-                    
-                            <div class="d-flex mb-2 align-items-center grid-gap-part">
-                                <div class="contact-info-icon">
-                                    <img src="/images/call.svg" alt="">
-                                </div>
-                                <div class="contact-info-number">
-                                    <a href="tel:+1(999)999-99-99" class="number">
-                                        +1(999)999-99-99
+                                <div class="contact-info-number"  v-for="item in globalData.contacts.phone">
+                                    <a  v-if="item.phone" :href="`tel:${item.phone}`" class="number mr-2">
+                                        {{  item.phone}}
                                     </a>
                                 </div>
                             </div>
